744d3ac802c4bd5d60884e3355cf5b8b2443ada9
VikingRenhammarMetus <viking.r.metus@gmail.com>
2023-10-04 13:41:29 +0200

    Add confusion matrix helper from Udemy course materials

--- TensorFlow/tensorflow_helpers.py
+++ TensorFlow/tensorflow_helpers.py
@@ -3,11 +3,11 @@ import pytz
 
 import matplotlib.pyplot as plt
 
-import tensorflow_hub as hub
+import itertools
+import numpy as np
+from sklearn.metrics import confusion_matrix
 
 from tensorflow.keras.callbacks import TensorBoard as TensorBoardCallback
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
 
 def create_tensorboard_callback(dir_name, experiment_name, timezone = "Europe/Stockholm"):
     """Construct a TensorBoard callback to be used in the TensorFlow fit() function.
@@ -26,30 +26,82 @@ def create_tensorboard_callback(dir_name, experiment_name, timezone = "Europe/St
     print(f"Saving TensorBoard log files to {log_dir}.")
     return tensorboard_callback
 
-def create_feature_extraction_model(feature_extractor_url, n_classes, input_shape):
-    """Creates a model, using the given feature extractor as the first N layers 
-        and adds a Dense output layer with the given number of classes.
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
+  """
+  From: https://github.com/mrdbourke/tensorflow-deep-learning/blob/main/extras/helper_functions.py
+  
+  Makes a labelled confusion matrix comparing predictions and ground truth labels.
 
-    Args:
-        feature_extractor_url (str): The TensorFlow Hub URL to fetch the model 
-            from.
+  If classes is passed, confusion matrix will be labelled, if not, integer class values
+  will be used.
 
-        n_classes (int, optional): The number of neurons in the output layer.
+  Args:
+    y_true: Array of truth labels (must be same shape as y_pred).
+    y_pred: Array of predicted labels (must be same shape as y_true).
+    classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+    figsize: Size of output figure (default=(10, 10)).
+    text_size: Size of output figure text (default=15).
+    norm: normalize values or not (default=False).
+    savefig: save confusion matrix to file (default=False).
+  
+  Returns:
+    A labelled confusion matrix plot comparing y_true and y_pred.
 
-        input_shape (list[int], optional): Shape of the input data to the 
-            feature extractor.
+  Example usage:
+    make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                          y_pred=y_preds, # predicted labels
+                          classes=class_names, # array of class label names
+                          figsize=(15, 15),
+                          text_size=10)
+  """  
+  # Create the confustion matrix
+  cm = confusion_matrix(y_true, y_pred)
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+  n_classes = cm.shape[0] # find the number of classes we're dealing with
 
-    Returns:
-        tensorflow.keras.models.Sequential: The Sequential model combining the 
-            feature extractor with a Dense output layer.
-    """
-    feature_extractor_layer = hub.KerasLayer(feature_extractor_url,
-                                            trainable = False,
-                                            name = "feature_extraction_layer",
-                                            input_shape = input_shape)
-    model = Sequential([feature_extractor_layer,
-                        Dense(n_classes, activation = "softmax", name = "output_layer")])
-    return model
+  # Plot the figure and make it pretty
+  fig, ax = plt.subplots(figsize=figsize)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
+  fig.colorbar(cax)
+
+  # Are there a list of classes?
+  if classes:
+    labels = classes
+  else:
+    labels = np.arange(cm.shape[0])
+  
+  # Label the axes
+  ax.set(title="Confusion Matrix",
+         xlabel="Predicted label",
+         ylabel="True label",
+         xticks=np.arange(n_classes), # create enough axis slots for each class
+         yticks=np.arange(n_classes), 
+         xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+         yticklabels=labels)
+  
+  # Make x-axis labels appear on bottom
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  # Set the threshold for different colors
+  threshold = (cm.max() + cm.min()) / 2.
+
+  # Plot the text on each cell
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    if norm:
+      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+    else:
+      plt.text(j, i, f"{cm[i, j]}",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+
+  # Save the figure to the current working directory
+  if savefig:
+    fig.savefig("confusion_matrix.png")
 
 def plot_metrics(history, metrics = ['loss', 'accuracy'], include_validation_data = True):
     """Plots each metric in a separate figure.
